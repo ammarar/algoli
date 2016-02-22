260d38abbe8644f2b4e10ce85d477a5875322807
Ammar Alrashed <ammar.alrashed@gmail.com>
2016-02-21 22:48:45 -0800

    LinkedList.java + Finished implementing the rest of the methods = Fixed some of the Node generic issues

--- src/main/java/me/algoli/list/LinkedList.java
+++ src/main/java/me/algoli/list/LinkedList.java
@@ -2,11 +2,9 @@ package me.algoli.list;
 
 import me.algoli.Iterator;
 
-/**
- * Created by ammar on 1/8/16.
- */
 public class LinkedList<E> implements List<E>, Queue<E>, Stack<E> {
 
+    private int length;
     private Node<E> head;
     private Node<E> tail;
 
@@ -20,10 +18,11 @@ public class LinkedList<E> implements List<E>, Queue<E>, Stack<E> {
         if (isEmpty()) {
             initialize(e);
         } else {
-            Node node = Node.create(e);
+            Node<E> node = Node.create(e);
             tail.append(node);
             tail = node;
         }
+        length++;
         return true;
     }
 
@@ -32,15 +31,16 @@ public class LinkedList<E> implements List<E>, Queue<E>, Stack<E> {
         if (isEmpty()) {
             initialize(e);
         } else {
-            Node node = Node.create(e);
+            Node<E> node = Node.create(e);
             head.prepend(node);
             head = node;
         }
+        length++;
         return true;
     }
 
     private void initialize(E e) {
-        Node node = Node.create(e);
+        Node<E> node = Node.create(e);
         this.head = node;
         this.tail = node;
     }
@@ -56,6 +56,7 @@ public class LinkedList<E> implements List<E>, Queue<E>, Stack<E> {
             if (runner != null) {
                 runner.append(Node.create(e));
                 result = true;
+                length++;
             }
         }
         return result;
@@ -68,100 +69,126 @@ public class LinkedList<E> implements List<E>, Queue<E>, Stack<E> {
 
     private Node<E> nodeAt(final int index) {
         int count = index;
-        Node runner = head;
+        Node<E> runner = head;
         for (; runner != null && count > 0; runner = runner.next) count--;
         return runner;
     }
 
     @Override
     public boolean remove(E e) {
-        return false;
+        Node<E> node = find(e);
+        if (node == null) {
+            return false;
+        }
+        node.remove();
+        length--;
+        return true;
+    }
+
+    private Node<E> find(E e) {
+        Node runner = head;
+        for(; runner != null && !runner.getVal().equals(e); runner = runner.next);
+        return runner;
     }
 
     @Override
     public void clear() {
-
+        head = null;
+        tail = null;
+        length = 0;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return length == 0;
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        Node<E> node = nodeAt(index);
+        if (node == null) {
+            return null;
+        }
+        node.setVal(element);
+        return element;
     }
 
     @Override
     public void push(E e) {
-
+        prepend(e);
     }
 
     @Override
     public E pop() {
-        return null;
+        Node<E> node = tail;
+        tail.remove();
+        return node.getVal();
     }
 
     @Override
     public E pop(int i) {
-        return null;
+        Node<E> node = nodeAt(i);
+        node.remove();
+        return node.getVal();
     }
 
     @Override
     public void enqueue(E e) {
-
+        append(e);
     }
 
     @Override
     public E dequeue() {
-        return null;
+        return pop(0);
     }
 
     @Override
     public E peek() {
-        return null;
+        if (head == null) {
+            throw new NullPointerException("List is empty");
+        }
+        return head.getVal();
     }
 
     @Override
     public int size() {
-        return 0;
+        return length;
     }
 
     @Override
     public Iterator iterator() {
-        return null;
+        return new LinkedListIterator();
     }
 
-    private static class Node<E> {
-        private E val;
-        private Node next;
-        private Node prev;
+    private static class Node<X> {
+        private X val;
+        private Node<X> next;
+        private Node<X> prev;
 
         private Node() {}
-        private Node(E val, Node next, Node prev) {
+        private Node(X val, Node<X> next, Node<X> prev) {
             this.val = val; this.next = next; this.prev = prev;
         }
 
-        public static Node create() {return new Node();}
-        public static <E> Node create(E e) {return new Node(e, null, null);}
-        public static <E> Node<E> create(E val, Node next, Node prev) {
+        public static <X> Node<X> create() {return new Node<>();}
+        public static <X> Node<X> create(X e) {return new Node<>(e, null, null);}
+        public static <X> Node<X> create(X val, Node<X> next, Node<X> prev) {
             return new Node<>(val, next, prev);
         }
 
-        public Node getNext() {
+        public Node<X> getNext() {
             return next;
         }
-        public E getVal() {
+        public X getVal() {
             return val;
         }
 
-        public Node getPrev() {
+        public Node<X> getPrev() {
             return prev;
         }
 
-        public Node append(Node node) {
-            Node after = node.getNext();
+        public Node<X> append(Node<X> node) {
+            Node<X> after = node.getNext();
             this.next = node;
             node.prev = this;
             if (after != null) {
@@ -171,7 +198,7 @@ public class LinkedList<E> implements List<E>, Queue<E>, Stack<E> {
             return node;
         }
 
-        public Node prepend(Node node) {
+        public Node<X> prepend(Node<X> node) {
             this.prev = node;
             node.next = this;
             return node;
@@ -190,5 +217,29 @@ public class LinkedList<E> implements List<E>, Queue<E>, Stack<E> {
             this.prev = null;
         }
 
+        public void setVal(X val) {
+            this.val = val;
+        }
+    }
+
+    private class LinkedListIterator implements Iterator<E> {
+
+        private Node<E> node;
+
+        public LinkedListIterator() {
+            node = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return node != null;
+        }
+
+        @Override
+        public E next() {
+            Node<E> curr = node;
+            node = node.getNext();
+            return curr.getVal();
+        }
     }
 }
