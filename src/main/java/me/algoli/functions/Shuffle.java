552b6e13632c6aab1cbca830f91b3906611771b5
ammar <ammar.alrashed@gmail.com>
2016-04-06 22:05:44 -0700

    Shell sort implementation and test. Also shuffle.

--- src/main/java/me/algoli/functions/Shuffle.java
+++ src/main/java/me/algoli/functions/Shuffle.java
@@ -2,6 +2,8 @@ package me.algoli.functions;
 
 import me.algoli.list.List;
 
+import java.util.Random;
+
 /**
  * Created by ammar on 2/22/16.
  */
@@ -10,6 +12,10 @@ public class Shuffle {
     private Shuffle() {};
 
     public static <T> void knuthShuffle(List<Comparable<T>> list) {
-
+        Random rand = new Random(100);
+        for (int i = 1; i < list.size(); i++) {
+            int random = rand.nextInt(i + 1);
+            list.exchange(i, random);
+        }
     }
 }
--- src/main/java/me/algoli/sort/ShellSort.java
+++ src/main/java/me/algoli/sort/ShellSort.java
@@ -9,6 +9,17 @@ public class ShellSort implements SortStrategy {
 
     @Override
     public <T extends Comparable<? super T>> void sort(List<T> list) {
+        if (!SortStrategy.needsSorting(list)) return;
+        int h = 1;
+        while (h < list.size()/3) { h = 3 * h + 1; }
 
+        while (h >= 1) {
+            for (int i = h; i<list.size(); i++) {
+                for (int j = i; j >= h && list.get(j).compareTo(list.get(j - h)) < 0; j -= h) {
+                    list.exchange(j, j - h);
+                }
+            }
+            h = h/3;
+        }
     }
 }
